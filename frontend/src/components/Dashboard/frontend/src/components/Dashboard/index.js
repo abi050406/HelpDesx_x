91e3657abaeb5a1bba114d42342ce9a2451c942e
katherine Garcia <142952143+abi050406@users.noreply.github.com>
2026-07-04 06:32:44 -0600

    Delete frontend/src/components/Dashboard/frontend/src/components/Dashboard/index.js

--- frontend/src/components/Dashboard/frontend/src/components/Dashboard/index.js
+++
@@ -1,7 +0,0 @@
-export { default as AdminDashboard } from './AdminDashboard';
-export { default as MetricsGrid } from './MetricsGrid';
-export { default as ChartsSection } from './ChartsSection';
-export { default as RightRail } from './RightRail';
-export { default as TicketWorkspace } from './TicketWorkspace';
-export { default as TicketQueue } from './TicketQueue';
-export { default as TicketDetail } from './TicketDetail';
